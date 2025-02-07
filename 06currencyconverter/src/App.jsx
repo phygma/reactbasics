0fa91101f2c161a19e59d5f1cc81e40d01796ab3
Aman <arora.aman203@gmail.com>
2025-02-07 09:49:44 +0530

    aise hi

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -7,9 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style = {{backgroundImage:  `url(https://images.pexels.com/photos/3483098/pexels-photo-3483098.jpeg)`}}>
+
+
+    </div>
   )
 }
 
---
+++ 06currencyconverter/src/components/inputbox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function inputbox({
+      label,
+      amount,
+      onAmountChange,
+      onCurrencyChange,
+      currencyOptions = [],
+      selectCurrency = "usd",
+      amountDisabled = false,
+      currencyDisabled = false,
+      className = "",
+}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default inputbox
