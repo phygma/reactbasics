31936cf5bf2791a51fa27d668b8c4999b2b53183
Aman <arora.aman203@gmail.com>
2025-02-20 21:03:49 +0530

    pic

--- 07reactrouter/src/components/Slider/Slider.jsx
+++ 07reactrouter/src/components/Slider/Slider.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Slider = () => {
       return (
-            <div className='flex justify-center'>
-                  <div className='left-btn'>
+            <div className='flex'>
+                  <div className='left-btn absolute  items-center justify-start'>
                         <button>{"<"}</button>
                   </div>
-                  <img src="https://www.contentgarden.in/PICTURES/030122/WATERMARK/20220103078L.jpg" alt="image" />
+                  <img src="https://www.contentgarden.in/PICTURES/030122/WATERMARK/20220103078L.jpg" alt="image"  className='justify-center'/>
                   
-                  <div className='right-btn'><button>{">"}</button></div>
+                  <div className='right-btn absolute items-center justify-end'><button>{">"}</button></div>
             </div>
       )
 }
